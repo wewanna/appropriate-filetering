5167385ffda4308ede008fb279ca1ea2efb83cb8
Cheolwan-Park <cheolwan.park552@gmail.com>
2019-07-12 14:54:58 +0900

    add next marking

--- labeling_helper.py
+++ labeling_helper.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import argparse
+from itertools import islice
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input')
@@ -9,24 +10,27 @@ parser.add_argument('start')
 args = parser.parse_args()
 
 start = int(args.start)
+if start < 1:
+    print('start should be greater than 0')
+    exit(-1)
 
 
 def clear():
     os.system('clear')
 
 
+count = start
 with open(args.input, 'r') as i_file:
     with open(args.output, 'a') as o_file:
         rd = csv.reader(i_file)
         wt = csv.writer(o_file)
-        for i in range(start):
-            rd = next(rd)
-        for line in rd:
+        for line in islice(rd, start-1, None):
             s = line[1]
             print(s)
             label = input()
             if label == 'q':
                 break
+            count += 1
             if label == '2':
                 continue
             label = int(label)
@@ -35,3 +39,4 @@ with open(args.input, 'r') as i_file:
                 exit(-1)
             wt.writerow([label, s])
             clear()
+        wt.writerow(['next', count])
